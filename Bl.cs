e9816391c0da6325ce69ef1dc7f6556ab884d538
Дмитрий <rabolan@mail.ru>
2019-12-17 16:54:40 +0300

    Добавьте файлы проекта.

---
+++ Bl.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace TimeUpdatesWF
+{
+  public  class Bl
+    {
+        /// <summary>
+        /// Класс для работы с логикой
+        /// </summary>
+        // https://www.youtube.com/watch?v=Ier5xem-TTA&list=PL0lO_mIqDDFWOMqSKFaLypANf1W7-o87q&index=5&t=594s
+        // //План
+        // Сделать автозагрузку времени из прпиртес, работающий устанвщик минут
+
+            private const string myServise = "W32Time";
+            private int UpdatesMinute = 60;
+
+            // Запуск службы
+            public void StartService(string serviceName = myServise)
+            {
+                ServiceController service = new ServiceController(serviceName);
+
+                // Проверяем не запущена ли служба
+                if (service.Status != ServiceControllerStatus.Running)
+                {
+                    // Запускаем службу
+                    service.Start();
+                    // В течении минуты ждём статус от службы
+                    service.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
+                    // Console.WriteLine("Служба была успешно запущена!");
+                }
+                else
+                {
+                    // Console.WriteLine("Служба уже запущена!");
+                }
+            }
+
+
+            // Останавливаем службу
+            public void StopService(string serviceName = myServise)
+            {
+                ServiceController service = new ServiceController(serviceName);
+
+                // Если служба не остановлена
+                if (service.Status != ServiceControllerStatus.Stopped)
+                {
+                    // Останавливаем службу
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromMinutes(1));
+                    //  Console.WriteLine("Служба была успешно остановлена!");
+                }
+                else
+                {
+                    //   Console.WriteLine("Служба уже остановлена!");
+                }
+            }
+
+
+            // Перезапуск службы
+            public void RestartService(string serviceName = myServise)
+            {
+                ServiceController service = new ServiceController(serviceName);
+                TimeSpan timeout = TimeSpan.FromMinutes(UpdatesMinute);
+
+                if (service.Status != ServiceControllerStatus.Stopped)
+                {
+                    //  Console.WriteLine("Перезапуск службы. Останавливаем службу...");
+                    // Останавливаем службу
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+                    //  Console.WriteLine("Служба была успешно остановлена!");
+                }
+                if (service.Status != ServiceControllerStatus.Running)
+                {
+                    //   Console.WriteLine("Перезапуск службы. Запускаем службу...");
+                    // Запускаем службу
+                    service.Start();
+                    service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                    //   Console.WriteLine("Служба была успешно запущена!");
+                }
+            }
+
+            public bool InitMinutes(int myMinutes)
+            {
+                UpdatesMinute = myMinutes;
+                // RestartService(myServise, myMinutes);
+                return true;
+            }
+
+            public int GettMinutes(int myMinutes)
+            {
+
+                return UpdatesMinute;
+            }
+
+        }
+    }
+
+
---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TimeUpdatesWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(396, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(372, 243);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(364, 217);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Автообновление времени";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(364, 217);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Синхронизация времени";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 343);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Автообновление времени";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
