106ae22a1bfb418eef00a46d1f7353694ac0fdc1
Дмитрий <rabolan@mail.ru>
2020-01-16 14:31:25 +0300

    рр

--- Bl.cs
+++ Bl.cs
@@ -471,7 +471,7 @@ namespace TimeUpdatesWF
         }
 
         /// <summary>
-        /// Поточнаякоманда сдм на обновление времени
+        /// Поточная команда сдм на обновление времени
         /// </summary>
         /// <returns></returns>
         public string ExecuteCommandAsAdmin()
